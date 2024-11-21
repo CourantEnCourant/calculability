b71f8cccff408a8919a6beaf6ae9ad4dc77184fb
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2024-11-21 21:28:38 +0100

    feat: add check_syntaxe func

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,3 +5,14 @@ def tokenize(ts_script: str) -> list[str]:
 	"""
 	return
 	
+	
+def check_syntaxe(ts_script: list[str]) -> bool:
+	"""
+	Returns True if program is valid, False (or raise) if not.
+	Check if a tokenized ts_script is a valid program of not
+	There are a few possible errors. The script needs to be ended, parenthesis have to match, a loop without ending condition, etc.
+	Feel free to elaborate if you have other errors in mind.
+	It will be nice if we can have a different message for different kind of problem.
+	"""
+	return
+	
