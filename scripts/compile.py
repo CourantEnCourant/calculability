3ed2d9b26dd74fd5a76cf5324ce98a830a356750
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2025-01-09 21:50:16 +0100

    feat: add compiler

---
+++ scripts/compile.py
@@ -0,0 +1,21 @@
+""""""
+import logging
+from tqdm import tqdm
+from pathlib import Path
+from typing import Sequence, Optional
+
+
+def main():
+    # Logger config
+    logging.basicConfig(level=logging.INFO)
+    pass
+    
+    
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument()
+    args = parser.parse_args()
+    
+    main()
+    
